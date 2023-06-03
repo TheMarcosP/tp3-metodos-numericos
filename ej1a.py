1cfb0b2afd1be27499383de820d18ae84c6b42b9
TheMarcosP <50151245+TheMarcosP@users.noreply.github.com>
2023-06-03 17:08:54 -0300

    ej1a creo que ahora esta bien

--- ej1a.py
+++ ej1a.py
@@ -1,57 +1,107 @@
-import os
 import numpy as np
+import os
 from PIL import Image
-import matplotlib.pyplot as plt
 
-# Directorio que contiene las imágenes
-directorio_imagenes = 'dataset_imagenes'
-
-# Lista para almacenar los vectores de las imágenes
-vectores_imagenes = []
+directorio_imagenes = "dataset_imagenes"
 
 # Obtener la lista de archivos en el directorio
-archivos = os.listdir(directorio_imagenes)
+lista_archivos = os.listdir(directorio_imagenes)
 
-# Iterar sobre los archivos del directorio
-for archivo in archivos:
-    # Comprobar si el archivo es una imagen
-    if archivo.endswith('.png') or archivo.endswith('.jpg') or archivo.endswith('.jpeg'):
-        # Leer la imagen utilizando la biblioteca PIL
-        ruta_imagen = os.path.join(directorio_imagenes, archivo)
-        imagen = Image.open(ruta_imagen)
-        
-        # Convertir la imagen a un arreglo de NumPy
-        arreglo_imagen = np.array(imagen)
-        
-        # Apilar el arreglo de la imagen como un vector y añadirlo a la lista
-        vector_imagen = arreglo_imagen.flatten()
-        vectores_imagenes.append(vector_imagen)
+lista_vectores_imgs = []
 
-# Apilar los vectores de imágenes en una matriz de NumPy
-matriz_imagenes = np.column_stack(vectores_imagenes)
+# Recorrer cada archivo de imagen en el directorio
+for i, archivo in enumerate(lista_archivos):
+    ruta_imagen = os.path.join(directorio_imagenes, archivo)
+    imagen = Image.open(ruta_imagen)
 
-# Realizar la descomposición SVD de la matriz
-U, S, V = np.linalg.svd(matriz_imagenes)
+    # Convertir la imagen en una matriz NumPy
+    matriz_imagen = np.asarray(imagen)
 
-# Número de primeras y últimas dimensiones a visualizar
-p = 30
+    p = matriz_imagen.shape[0]  # asumiendo que la imagen es cuadrada (p x p)
+    
+    # Transformar la imagen en un vector
+    vector_imagen = matriz_imagen.reshape(p*p)
 
-# Visualizar las primeras dimensiones (autovectores)
-plt.figure(figsize=(10, 4))
-for i in range(p):
-    plt.subplot(2, p, i+1)
-    autovector = U[:, i].reshape(arreglo_imagen.shape)
-    plt.imshow(autovector, cmap='gray')
-    plt.axis('off')
-    plt.title(f'Autovector {i+1}')
+    # crea un vector de largo p*p con i en cada posicion
+    # vector_imagen = np.full(p*p, i)
+    
+    lista_vectores_imgs.append(vector_imagen)
 
-# Visualizar las últimas dimensiones (autovectores)
-for i in range(p):
-    plt.subplot(2, p, p+i+1)
-    autovector = U[:, -i-1].reshape(arreglo_imagen.shape)
-    plt.imshow(autovector, cmap='gray')
-    plt.axis('off')
-    plt.title(f'Autovector {matriz_imagenes.shape[1]-i}')
 
-plt.tight_layout()
-plt.show()
+matriz = np.array(lista_vectores_imgs).T
+
+
+A1 = matriz[:, 4]
+
+# print(A1)
+# print(A1.shape)
+
+# reshape as p*p
+A1 = A1.reshape(p, p)
+
+A1 = Image.fromarray(A1)
+A1.save("pruebaZZ"+ ".png")
+
+
+
+# # print(matriz)
+# # print(matriz.shape)
+
+
+
+U, S, Vt = np.linalg.svd(matriz)
+# print(U.shape, S.shape, Vt.shape)
+
+# # guardar reconstruccion de la imagen
+
+
+
+# # # keep the last k singular values
+# # k = 16
+# # # keep only the first k singular values
+
+# # S = S[:k]
+# # # keep only the first k columns of U
+# # U = U[:, :k]
+# # # keep only the first k rows of Vt
+# # Vt = Vt[:k, :]
+# # # reconstruct the matrix
+
+# # matriz_reconstruida = U.dot(np.diag(S)).dot(Vt)
+
+# # # for i in range(16):
+# # #     # get the i-th column of the reconstructed matrix
+# # #     imagen_reconstruida = matriz_reconstruida[:, i]
+# # #     # reshape the vector to a matrix
+# # #     imagen_reconstruida = imagen_reconstruida.reshape(p, p)
+
+# # #     imagen_reconstruido = Image.fromarray(imagen_reconstruida)
+# # #     imagen_reconstruido = imagen_reconstruido.convert("RGB")
+# # #     imagen_reconstruido.save("aare" + str(i) + ".png")
+
+
+# # guardar las Visualizar en forma matricial p×p las primeras y las últimas dimensiones (autovectores) de la descomposición
+# # obtenida.
+
+# # primer columna de U
+
+for i in range(18):
+    U1 = U[i,:]
+
+    # # print(U1)
+    # # print(U1.shape)
+
+    # # reshape as p*p
+    U1 = U1.reshape(p, p)
+
+    from PIL import ImageOps
+
+    U1 = U1 - np.min(U1)  # Asegurar valores no negativos
+    U1 = U1 / np.max(U1)  # Normalizar entre 0 y 1
+    U1 = U1 * 255  # Escalar al rango 0-255
+    U1 = U1.astype(np.uint8)  # Convertir a tipo entero sin signo de 8 bits
+
+    U1_image = Image.fromarray(U1)
+    # U1_image = ImageOps.invert(U1_image)  # Invertir los colores de la imagen
+    U1_image.save(f"pruebaU{i}.png")
+# # pca es hacer svd y ver las columnas de esa matriz
